37094e1b4d92c36bc7987a75d5db9a00718bbd6e
Andre Pereira <andre.pereira@mprs.mp.br>
2024-06-30 19:29:46 -0300

    feat: criação schema e models - Entidade centro de treinamento

--- backend/categorias/schemas.py
+++ backend/categorias/schemas.py
@@ -1,8 +1,8 @@
 from typing import Annotated
-from pydantic import Field, PositiveFloat
+from pydantic import Field
 
 from backend.contrib.schemas import BaseSchema
 
 class Categoria(BaseSchema):
-    nome: Annotated[str, Field(description='Nome da categoria', examples='Scale', max_length=10)]
+    nome: Annotated[str, Field(description='Nome da categoria', example='Scale', max_length=10)]
     
